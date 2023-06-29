2eb864581ea59006a284594b7673c0997d4b8f6b
sosamatias1 <sosamatias171@gmail.com>
2023-06-29 19:01:15 -0300

    se agregan headers en el cors

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ import categorySpeed from "./seed/categorySeed.js"
 import cookieParser from "cookie-parser";
 import cors from 'cors'
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5174',
+    credentials: true
+}))
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
